45ada122accb99b28c74ba23458bc372bbbebd70
Timi Ombe <timmyombe@gmail.com>
2022-09-22 15:10:28 +0100

    part 2 - create hello world react application + mockserver

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+
+app.get('/', (req, res) => {
+    res.send("Hello World!")
+})
